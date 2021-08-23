4e688c7e4d41a4dacabba7729e130c783a183d4b
Jonas Schiefner <jonas.schiefner@zweitag.de>
2021-08-23 18:58:06 +0200

    implement preference button functionality in controller

--- advancedbrightnesssliderpreferences/ABSRootListController.m
+++ advancedbrightnesssliderpreferences/ABSRootListController.m
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 #import "ABSRootListController.h"
+#import <spawn.h>
 
 @implementation ABSRootListController
 
@@ -9,6 +10,23 @@
 	}
 
 	return _specifiers;
+}
+
+// credits: https://github.com/Muirey03/PowerModule/blob/master/Source/RespringButtonController.m
+-(void)respring {
+	pid_t pid;
+	int status;
+	const char* args[] = {"sbreload", NULL};
+	posix_spawn(&pid, "/usr/bin/sbreload", NULL, NULL, (char* const*)args, NULL);
+	waitpid(pid, &status, WEXITED);
+}
+
+-(void)openGithub {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/jschiefner/advanced-brightness-slider-tweak/"] options:@{} completionHandler:nil];
+}
+
+-(void)openPaypal {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://paypal.me/JonasSchiefner"] options:@{} completionHandler:nil];
 }
 
 @end
