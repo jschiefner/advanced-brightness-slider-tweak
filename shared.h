05e9022bd3faf6d4e98d463cca96067b484e5aea
Jonas Schiefner <jonas.schiefner@zweitag.de>
2021-09-27 17:16:13 +0200

    add interfaces to shared.h and use import guard

--- shared.h
+++ shared.h
@@ -1,2 +1,35 @@
-extern "C" void initNative(NSDictionary* bundleDefaults);
-extern "C" void initPrysm(NSDictionary* bundleDefaults);
+#ifndef ADVANCED_BRIGHTNESS_SLIDER_SHARED_H
+#define ADVANCED_BRIGHTNESS_SLIDER_SHARED_H
+
+extern "C" {
+  void initNative();
+  void initPrysm();
+}
+
+@interface CCUIContinuousSliderView : UIControl
+@property (nonatomic) BOOL isBrightnessSlider;
+-(void)_handleValueChangeGestureRecognizer:(UIPanGestureRecognizer*)recognizer;
+-(void)setGlyphState:(NSString*)arg1;
+-(void)setValue:(float)arg1;
+@end
+
+@interface CCUICAPackageDescription : NSObject
+-(NSURL *)packageURL;
+@end
+
+@interface CCUICAPackageView : UIView
+@property CCUICAPackageDescription* packageDescription;
+-(void)setPackageDescription:(CCUICAPackageDescription*)arg1;
+-(void)setStateName:(NSString*)arg1;
+@end
+
+@interface PrysmSliderViewController : UIViewController
+@property (nonatomic,retain) CCUICAPackageView * packageView;
+@property (assign,nonatomic) int style;
+-(BOOL)isBrightnessSlider;
+-(void)setProgressValue:(double)arg1 animated:(BOOL)arg2;
+-(void)setPackageOverlayView:(CCUICAPackageView *)arg1;
+-(void)move:(UIPanGestureRecognizer*)recognizer;
+@end
+
+#endif // ADVANCED_BRIGHTNESS_SLIDER_SHARED_H
