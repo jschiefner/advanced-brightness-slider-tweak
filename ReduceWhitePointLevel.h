3acfeee052c7fbdedf67394e8620f5c2168f2162
Nguyễn Anh Sáng <sn15062008@gmail.com>
2023-08-19 19:34:58 +0700

    add comment

--- ReduceWhitePointLevel.h
+++ ReduceWhitePointLevel.h
@@ -5,8 +5,9 @@ extern "C" {
 void MADisplayFilterPrefSetReduceWhitePointIntensity(CGFloat intenity);
 CGFloat MADisplayFilterPrefGetReduceWhitePointIntensity();
 
+// function to change brightness
 void BKSDisplayBrightnessSet(float amount, int _unknown);
 
 #if defined __cplusplus
 };
-#endif
+#endif 
